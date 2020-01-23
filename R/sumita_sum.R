a51357e8561a9355110270420fa5f3b3adeb2944
PaulLulo <60224233+PaulLulo@users.noreply.github.com>
2020-01-23 10:57:15 -0500

    Add files via upload

---
+++ R/sumita_sum.R
@@ -0,0 +1,13 @@
+#' @title Sum two numbers
+#' @description  this is package is a pruebita
+#' @param  c(a,b)
+#' @return  the sumita
+#' @examples sumita_sum(a,b)
+#' @export sumita_sum
+
+sumita_sum <- function(a,b) {
+
+  d<-a+b
+  print(d)
+}
+
